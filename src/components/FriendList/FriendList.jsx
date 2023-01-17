1fc7b45156449e6a38940f2bc2722f074fa17f57
Yura <104792276+Yura-111@users.noreply.github.com>
2023-01-17 15:24:05 +0200

    Update FriendList.jsx

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -7,17 +7,18 @@ import { Button } from 'components/Button/Button';
 import { deleteContact } from 'redux/contactsSlice';
 
 export const ContactList = () => {
-  const contactList = useSelector(state => state.contacts);
+  const contactList = useSelector(state => state.contacts.contacts);
   const filter = useSelector(state => state.filter);
   const dispatch = useDispatch();
 
-  const filteredContacts = contactList.filter(contact =>
-    contact.name.toLowerCase().includes(filter.toLowerCase().trim())
-  );
+  const filteredContacts = filter === "" ? contactList :
+  contactList.filter(contact => {
+    return contact.name.toLowerCase().includes(filter.toLowerCase())
+  });
 
   return (
     <StyledList>
-      {filteredContacts.map(({ id, name, number }) => (
+      {filteredContacts.length > 0 && filteredContacts.map(({ id, name, number }) => (
         <FriendItem key={id}>
           <FriendDescription>
             {name}: {number}
